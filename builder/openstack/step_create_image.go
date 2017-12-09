2a21912c93a211e9abfd3ae185593658117a4880
Pierre CARRE <pierre.carre@orange.com>
2017-12-09 20:18:30 +0100

    Fix error details in messages

--- builder/openstack/step_create_image.go
+++ builder/openstack/step_create_image.go
@@ -22,7 +22,7 @@ func (s *stepCreateImage) Run(state multistep.StateBag) multistep.StepAction {
 	// We need the v2 compute client
 	client, err := config.computeV2Client()
 	if err != nil {
-		err = fmt.Errorf("Error initializing compute client: %s", err)
+		err = fmt.Errorf("Error initializing compute client: %s", err.Error())
 		state.Put("error", err)
 		return multistep.ActionHalt
 	}
@@ -34,7 +34,7 @@ func (s *stepCreateImage) Run(state multistep.StateBag) multistep.StepAction {
 		Metadata: config.ImageMetadata,
 	}).ExtractImageID()
 	if err != nil {
-		err := fmt.Errorf("Error creating image: %s", err)
+		err := fmt.Errorf("Error creating image: %s", err.Error())
 		state.Put("error", err)
 		ui.Error(err.Error())
 		return multistep.ActionHalt
@@ -47,7 +47,7 @@ func (s *stepCreateImage) Run(state multistep.StateBag) multistep.StepAction {
 	// Wait for the image to become ready
 	ui.Say(fmt.Sprintf("Waiting for image %s (image id: %s) to become ready...", config.ImageName, imageId))
 	if err := WaitForImage(client, imageId); err != nil {
-		err := fmt.Errorf("Error waiting for image: %s", err)
+		err := fmt.Errorf("Error waiting for image: %s", err.Error())
 		state.Put("error", err)
 		ui.Error(err.Error())
 		return multistep.ActionHalt
